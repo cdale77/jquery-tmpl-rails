1a378f902dd341fc24f8aec3311e850ccc37e250
Brad Johnson <cdale77@gmail.com>
2016-11-23 16:09:24 -0800

    wip

--- lib/jquery-tmpl-rails/engine.rb
+++ lib/jquery-tmpl-rails/engine.rb
@@ -8,8 +8,8 @@ module JqueryTmplRails
     initializer "sprockets.jquery_templates", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
       #app.assets.register_engine(".tmpl", JqueryTemplate)
-      app.assets.configure do |env|
-        env.register_mime_type "application/javascript", extensions: %w(.tmpl)
+      app.config.assets.configure do |env|
+        env.register_mime_type "text/tmpl", extensions: %w(.tmpl)
         env.register_transformer "text/tmpl", "text/html", JqueryTemplate
       end
     end
